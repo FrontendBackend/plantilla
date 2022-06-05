e1f4aa2dad04c1f8eb75b63bfdedef1559b480ba
juanvaleriomayta <juanvalerio785@gmail.com>
2022-06-04 21:57:08 -0500

    Nueva plantilla

---
+++ plantilla-frontend/src/app/theme/customizer/customizer.component.ts
@@ -0,0 +1,45 @@
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { AppSettings, SettingsService } from '@core';
+import { CdkDragStart } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-customizer',
+  templateUrl: './customizer.component.html',
+  styleUrls: ['./customizer.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class CustomizerComponent {
+  @Output() optionsChange = new EventEmitter<AppSettings>();
+
+  options = this.settings.getOptions();
+  opened = false;
+  dragging = false;
+
+  constructor(private settings: SettingsService) {}
+
+  handleDragStart(event: CdkDragStart) {
+    this.dragging = true;
+  }
+
+  openPanel(event: MouseEvent) {
+    if (this.dragging) {
+      this.dragging = false;
+      return;
+    }
+    this.opened = true;
+  }
+
+  closePanel() {
+    this.opened = false;
+  }
+
+  togglePanel() {
+    this.opened = !this.opened;
+  }
+
+  sendOptions() {
+    // eslint-disable-next-line no-debugger
+    debugger;
+    this.optionsChange.emit(this.options);
+  }
+}
